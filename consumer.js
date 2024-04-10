a62f67076d105eb0cc49362278115e19565fdd06
akshaykanherkarzype <akshay.kanherkar@getzype.com>
2024-04-10 12:54:17 +0530

    updated kafka

--- consumer.js
+++ consumer.js
@@ -1,7 +1,7 @@
 const kafka = require('kafka-node');
 const Consumer = kafka.Consumer;
-const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
-const consumer = new Consumer(client, [{ topic: 'test-topic', partition: 0 }]);
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:29092' });
+const consumer = new Consumer(client, [{ topic: 'test-topic-may-kafka', partition: 0 }]);
 
 consumer.on('message', (message) => {
   console.log('Received message:', message.value);
--- producer.js
+++ producer.js
@@ -1,13 +1,13 @@
 const kafka = require('kafka-node');
 const Producer = kafka.Producer;
-const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:29092' });
 const producer = new Producer(client);
 
 producer.on('ready', () => {
   console.log('Kafka producer is ready');
 
   // Send a message to a Kafka topic
-  const payloads = [{ topic: 'test-topic', messages: 'Hello, Kafka!' }];
+  const payloads = [{ topic: 'test-topic-may-kafka', messages: 'Hello, Kafka!' }];
   producer.send(payloads, (err, data) => {
     if (err) {
       console.error('Error sending message:', err);
